594aac4ac887f1ebd517942dae4d005089db2599
The Vault <114878799+crystal-coding-time@users.noreply.github.com>
2022-12-06 18:27:02 -0500

    added clock to code

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,3 +1,4 @@
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -20,4 +21,13 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
 });
+
+var today = dayjs();
+
+function time(){
+$("#currentDay").text(today.format('dddd, MMMM D YYYY, h:mm:ss a'));
+}
+
+time();
