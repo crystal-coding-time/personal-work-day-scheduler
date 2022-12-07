c3334bcd01634c4d25135d4584c68bb813f905ad
The Vault <114878799+crystal-coding-time@users.noreply.github.com>
2022-12-07 14:16:57 -0500

    Testing adding to local storage functionality

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -7,8 +7,13 @@ $("#currentDay").text(today.format('dddd, MMMM D YYYY, h:mm:ss a'));
 
 time();
 
-// Selected Elements 
-var saveBtn = $('#saveBtn');
+// The belowe code is the saveBtn click listener which saves the text entered to local storage 
+$(".saveBtn").on("click", function () {
+  var text = $(this).siblings("description").val();
+  var time = $(this).parent().attr("id");
+
+  localStorage.setItem(time, text);
+})
 
 
 $(function () {
