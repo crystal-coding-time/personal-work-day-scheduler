6b9fce88d699850366fb531257b13991bddd726a
The Vault <114878799+crystal-coding-time@users.noreply.github.com>
2022-12-07 18:48:41 -0500

    fixed save button and local storage issue, still working on dynamic Bootstrap

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -2,18 +2,20 @@
 var today = dayjs();
 
 function time(){
-$("#currentDay").text(today.format('dddd, MMMM D YYYY, h:mm:ss a'));
+$("#currentDay").text(today.format('dddd, MMM D YYYY'));
 }
 
 time();
 
-// The belowe code is the saveBtn click listener which saves the text entered to local storage 
-$(".saveBtn").on("click", function () {
-  var text = $(this).siblings("description").val();
+// The below code is the saveBtn click listener which saves the text entered to local storage 
+
+$(".saveBtn").on("click", function() {
+
+  var value = $(this).siblings(".description").val();
   var time = $(this).parent().attr("id");
 
-  localStorage.setItem(time, text);
-})
+  localStorage.setItem(time, value );
+});
 
 
 $(function  timeManager() {
@@ -39,7 +41,22 @@ $(function  timeManager() {
     }
 
 console.log(timeBlock);
+
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+$("#hour18 .description").val(localStorage.getItem("hour18"));
+
 timeManager();
   })
 });
 
+  // Get item from local storage if any
+  
+ 
