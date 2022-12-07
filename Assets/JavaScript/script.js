ac1b7510645bc59ba97730c3ba590fa55dff5ce7
The Vault <114878799+crystal-coding-time@users.noreply.github.com>
2022-12-07 11:16:21 -0500

    Adding to JavaScript file

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,3 +1,8 @@
+// Selected Elements 
+var saveBtn = $('#saveBtn');
+
+// saveBtn.on('click' )
+
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
