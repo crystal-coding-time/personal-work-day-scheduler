775c42a226df908be4c84c48a3fcf1be2c4c7ce0
The Vault <114878799+crystal-coding-time@users.noreply.github.com>
2022-12-08 13:11:14 -0500

    fixed naughty function

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -18,45 +18,45 @@ $(".saveBtn").on("click", function() {
 });
 
 
-$(function  timeManager() {
-// Check the current hour
-  var currentTime = today.hour()
-  console.log(currentTime); // testing the varible pulls the correct data
-
-  $(".time-block").each( function () {
+function  timeManager() {
+  // Check the current hour
+    var currentTime = today.hour()
+    console.log(currentTime); // testing the varible pulls the correct data
   
-    var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
-
-    if (timeBlock < currentTime ) {
-      $(this).removeClass("future");
-      $(this).removeClass("present");
-      $(this).addClass("past");
-    } else if (timeBlock === currentTime) {
-      $(this).removeClass("future");
-      $(this).removeClass("past");
-      $(this).addClass("present");
-    } else {
-      $(this).removeClass("past");
-      $(this).removeClass("present");
-      $(this).addClass("future");
-    }
-
-  })
-// Get item from local storage if any
+    $(".time-block").each( function () {
+    
+      var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
   
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
-$("#hour16 .description").val(localStorage.getItem("hour16"));
-$("#hour17 .description").val(localStorage.getItem("hour17"));
-$("#hour18 .description").val(localStorage.getItem("hour18"));
-
-  timeManager();
-});
-
+      if (timeBlock < currentTime ) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      } else if (timeBlock === currentTime) {
+        $(this).removeClass("future");
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+  
+    })
+  // Get item from local storage if any
+  }
+  
+  
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
+  $("#hour18 .description").val(localStorage.getItem("hour18"));
+  
+    timeManager();
   
  
